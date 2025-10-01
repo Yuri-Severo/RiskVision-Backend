1457a62c87d4c24752bfcbc95cf7320d51a13283
Yuri Severo <ryurisevero@gmail.com>
2025-10-01 10:54:56 -0300

    fix(historyModel): add 'Date' column

--- src/models/historyModel.py
+++ src/models/historyModel.py
@@ -14,4 +14,5 @@ class History(Base):
     volume = Column(Integer, nullable=False)
     dividends = Column(Float, nullable=False)  
     stock_splits = Column(Float, nullable=False)
+    date = Column(DateTime, nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
