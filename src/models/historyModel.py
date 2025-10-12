912fedc3d92a24d63859054a52237e28b78688a9
Yuri Severo <ryurisevero@gmail.com>
2025-10-12 15:20:46 -0300

    refactor(historyModel): adjust columns types

--- src/models/historyModel.py
+++ src/models/historyModel.py
@@ -1,11 +1,13 @@
 from sqlalchemy import Column, Integer, String, DateTime, Float
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
 from database import Base
+import uuid
 
 class History(Base):
     __tablename__ = "history"
 
-    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
     ticker_name = Column(String, nullable=False)
     open = Column(Float, nullable=False)
     high = Column(Float, nullable=False)
@@ -14,5 +16,5 @@ class History(Base):
     volume = Column(Integer, nullable=False)
     dividends = Column(Float, nullable=False)  
     stock_splits = Column(Float, nullable=False)
-    date = Column(DateTime, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    date = Column(DateTime(timezone=False), nullable=False)
+    created_at = Column(DateTime(timezone=False), server_default=func.now())
